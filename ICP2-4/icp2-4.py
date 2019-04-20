f6782e6a700d3c074178e66fdfa15e745d6e71bd
Kenton Hanifl <kwhv23@mail.umkc.edu>
2019-04-19 20:33:10 -0500

    icp 2-4

---
+++ ICP2-4/icp2-4.py
@@ -0,0 +1,6 @@
+import keras as ks
+from keras.datasets import cifar10
+
+cifar = cifar10.load_data()
+
+
--- ICP5/icp5.py
+++ ICP5/icp5.py
@@ -26,42 +26,55 @@ dataset = pd.read_csv('C:/Users/Kenton/Desktop/School/PythonDeepLearning/Python_
 
 x = dataset.iloc[:,[header1,header2]]
 
-x = preprocessing.normalize(x)
-
-scaler = preprocessing.StandardScaler()
-
-scaler.fit(x)
-X_scaled_array = scaler.transform(x)
-X_scaled = pd.DataFrame(X_scaled_array, columns = ['Apps','F.Undergrad'])
-
-nclusters = 5 # this is the k in kmeans
+##x = preprocessing.normalize(x)
+##
+##scaler = preprocessing.StandardScaler()
+##
+##scaler.fit(x)
+##X_scaled_array = scaler.transform(x)
+##X_scaled = pd.DataFrame(X_scaled_array, columns = ['Apps','F.Undergrad'])
+##
+##X_scaled = np.square(X_scaled)
+##X_scaled = 10*X_scaled
+nclusters = 2 # this is the k in kmeans
 seed = 0
 
-km = KMeans(n_clusters=nclusters, random_state=seed)
-km.fit(X_scaled)
+mdl = KMeans(n_clusters=nclusters, random_state=seed).fit(x)
 
 # predict the cluster for each data point
-y_cluster_kmeans = km.predict(X_scaled)
+y_cluster_kmeans = mdl.predict(x)
 
 d = pd.DataFrame(y_cluster_kmeans)
-print(d)
+##print(X_scaled)
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Create plot
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)#, axisbg="1.0")
+#fig = plt.figure()
+#ax = fig.add_subplot(1, 1, 1)#, axisbg="1.0")
 LABEL_COLOR_MAP = {0 : 'red',
                    1 : 'green',
                    2 : 'black',
                    3 : 'blue',
                    4 : 'yellow'
                    }
+
 color = [LABEL_COLOR_MAP[i] for i in d[0]]
-ax.scatter(x[:,0], x[:,1], alpha=0.8, c=color)
+plt.scatter(x["Apps"], x["F.Undergrad"], alpha=0.8, c=color)
  
 plt.title('Matplot scatter plot')
-plt.legend(loc=2)
 plt.show()
 
+wcss = []
+##elbow method to know the number of clusters
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1,11),wcss)
+plt.title('the elbow method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('Wcss')
+plt.show()
